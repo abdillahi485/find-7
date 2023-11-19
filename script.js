ba04deed392bc1412e262e0939216077e67d7709
Abdillahi485 <daakircadde143@gmail.com>
2023-11-19 15:37:35 +0300

    solved

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+function findSeven(numbers){
+for (let item of numbers){
+    if(item===7){
+        console.log('found')
+    }else(console.log('not found'))
+}
+}
+findSeven([1, 2, 3, 4, 5, 6, 7])
+findSeven([8, 6, 33, 100])
+findSeven([2, 55, 60, 97, 86])
